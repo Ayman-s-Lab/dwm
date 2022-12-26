e8c43a0eb05dd37c60c63ba97d6550567c649234
AymanLar <larourayman@gmail.com>
2022-12-26 18:12:50 +0100

    change bar size to match dmenu

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=14" };
+static const char *fonts[]          = { "JetBrains Mono:size=13" };
 static const char dmenufont[]       = "JetBrains Mono:size=13";
 //static const char col_gray1[]       = "#222222";
 //static const char col_gray2[]       = "#444444";
